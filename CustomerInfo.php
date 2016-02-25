bb32d441e95220f614df56f24fbd1e5698b61da2
Iain Scott <odyssey3007@hotmail.com>
2016-02-25 00:21:21 +0000

    commit damn it!!!

--- CustomerInfo.php
+++ CustomerInfo.php
@@ -24,7 +24,87 @@
 <body>
 <main>
 
-<h4>take this out later...</h4>
+
+
+
+
+
+    <form>
+
+        <table>
+
+            <tr><td> <p>Please complete the form:</p></td></tr>
+
+            <tr><td>
+                    <label for="title">Title:</label></td>
+                <td><select name="title">
+                        <option value="">Select Title</option>
+                        <option value="Mr">Mr</option>
+                        <option value="Mrs">Mrs</option>
+                        <option value="Miss">Miss</option>
+                        <option value="Ms">Ms</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td><label for="firstname">First Name:</label></td>
+                <td><input type="text" name="firstname" value="Enter your First Name" required /></td>
+            </tr>
+            <tr>
+                <td><label for="surname">Surname:</label></td>
+                <td><input type="text" name="surname" value="Enter your Surname" required /></td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="email">Email:</label></td>
+                <td><input type="text" name="email" placeholder="email" size="30" maxlength="50" required /></td>
+            </tr>
+
+
+
+
+            <tr><td>
+                    <label for="address">Address:</label></td>
+                <td><input type="text" name="address" placeholder=" Enter first line of your address" size="30" maxlength="50" required /></td>
+            </tr>
+
+            <tr><td>
+                    <label for="address2">Address Line 2:</label></td>
+                <td><input type="text" name="address2" placeholder=" Enter second line of your address" size="30" maxlength="50" required /></td>
+            </tr>
+
+
+            <tr><td>
+                    <label for="telephone">Telephone:</label></td>
+                <td><input type="text" name="telephone" placeholder=" Enter your telephone number" size="20" maxlength="20" required /></td>
+            </tr>
+
+            <tr>
+                <td><input type="submit" value="Submit" /></td>
+            </tr>
+        </table></form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <h4>take this out later...</h4>
     <div class="main">
         <h3 class="pagetitle" >Here are your search results...</h3>
 
@@ -52,63 +132,9 @@ NEWHTML;
     ?>
 </div>
 
-    <section>
-
-    <form>
-
-        <table>
-
-            <tr><td> <p>Please complete the form:</p></td></tr>
-
-            <tr><td>
-                <label for="title">Title:</label></td>
-                <td><select name="title">
-                    <option value="">Select Title</option>
-                    <option value="Mr">Mr</option>
-                    <option value="Mrs">Mrs</option>
-                    <option value="Miss">Miss</option>
-                    <option value="Ms">Ms</option>
-                </select>
-                </td>
-            </tr>
-            <tr>
-                <td><label for="firstname">First Name:</label></td>
-                <td><input type="text" name="firstname" value="Enter your First Name" required /></td>
-            </tr>
-            <tr>
-                <td><label for="surname">Surname:</label></td>
-                <td><input type="text" name="surname" value="Enter your Surname" required /></td>
-            </tr>
-            <tr>
-                <td>
-                    <label for="email">Email:</label></td>
-                <td><input type="text" name="email" placeholder="email" size="30" maxlength="50" required /></td>
-            </tr>
 
 
 
-
-            <tr><td>
-                <label for="address">Address:</label></td>
-                <td><input type="text" name="address" placeholder=" Enter first line of your address" size="30" maxlength="50" required /></td>
-            </tr>
-
-            <tr><td>
-                <label for="address2">Address Line 2:</label></td>
-                <td><input type="text" name="address2" placeholder=" Enter second line of your address" size="30" maxlength="50" required /></td>
-            </tr>
-
-
-            <tr><td>
-                <label for="telephone">Telephone:</label></td>
-                <td><input type="text" name="telephone" placeholder=" Enter your telephone number" size="20" maxlength="20" required /></td>
-            </tr>
-
-            <tr>
-                <td><input type="submit" value="Submit" /></td>
-            </tr>
-        </table></form>
-    </section>
 </main>
 <footer>
 
