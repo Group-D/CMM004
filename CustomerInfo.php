63376a8157ed163d52b024dbbb85a861824c49c2
iainscott-rgu <odyssey3007@hotmail.com>
2016-02-25 13:10:56 +0000

    Commit damn it!!

--- CustomerInfo.php
+++ CustomerInfo.php
@@ -113,7 +113,7 @@ NEWHTML;
         }
         $newhtml2 +=
             <<<NEWHTML
-<p>According to our database, your search of: <strong>{$row[city]}</strong> has returned the following results: </p>
+<p>According to our database, your search of: <strong>{$city}</strong> has returned the following results: </p>
 
 NEWHTML;
 
